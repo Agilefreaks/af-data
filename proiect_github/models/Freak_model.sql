8bff6f1f92c4d7874a28d81b7aaeb3887b8071a6
Gabor-Adrian <adigabor98@yahoo.com>
2025-06-07 16:34:55 +0300

    PUSH

--- proiect_github/models/Freak_model.sql
+++ proiect_github/models/Freak_model.sql
@@ -1,8 +1,8 @@
 with Freaks as (
-    select "FREAK FULL NAME"
+    select "FREAK FULL NAME","EMAIL ADDRESS"
 
     from {{ source('sursa_snowflake', 'FREAKS_EMAIL_ADDRESSES') }}
 )
 
-select "FREAK FULL NAME"
+select "FREAK FULL NAME","EMAIL ADDRESS"
 from Freaks
