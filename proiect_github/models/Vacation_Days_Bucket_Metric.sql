621db120cd4e30e2db5953b93e7554bdab7345c0
Gabor-Adrian <adigabor98@yahoo.com>
2025-06-14 11:12:59 +0300

    Solved Snowflake

--- proiect_github/models/Vacation_Days_Bucket_Metric.sql
+++ proiect_github/models/Vacation_Days_Bucket_Metric.sql
@@ -1,8 +1,20 @@
-SELECT 
-    TO_CHAR(TO_DATE("START DATE", 'DD/MM/YYYY'), 'MM/YYYY') AS LUNA,
-    "VACATION TYPE",
-    COUNT(*) AS NUMAR_CONCEDII
-FROM {{ref('Vacation_model')}}
-WHERE "VACATION CANCELLED?" IS NULL
-GROUP BY TO_CHAR(TO_DATE("START DATE", 'DD/MM/YYYY'), 'MM/YYYY'), "VACATION TYPE"
-ORDER BY LUNA, "VACATION TYPE"
+SELECT
+    "EMAIL ADDRESS" AS email,
+    EXTRACT(YEAR FROM TO_DATE("START DATE", 'DD/MM/YYYY')) AS year,
+    SUM(DATEDIFF(
+        'day',
+        TO_DATE("START DATE", 'DD/MM/YYYY'),
+        TO_DATE("END DATE", 'DD/MM/YYYY')
+    ) + 1) AS total_days_off,
+
+    CASE
+        WHEN SUM(DATEDIFF('day', TO_DATE("START DATE", 'DD/MM/YYYY'), TO_DATE("END DATE", 'DD/MM/YYYY')) + 1) <= 10 THEN '0-10 vacation days'
+        WHEN SUM(DATEDIFF('day', TO_DATE("START DATE", 'DD/MM/YYYY'), TO_DATE("END DATE", 'DD/MM/YYYY')) + 1) BETWEEN 11 AND 21 THEN '11-21 vacation days'
+        WHEN SUM(DATEDIFF('day', TO_DATE("START DATE", 'DD/MM/YYYY'), TO_DATE("END DATE", 'DD/MM/YYYY')) + 1) BETWEEN 22 AND 35 THEN '22-35 vacation days'
+        ELSE '35+ vacation days'
+    END AS days_off_category
+
+FROM Vacation_model
+WHERE "VACATION TYPE" IS NOT NULL
+GROUP BY email, year
+ORDER BY email, year
